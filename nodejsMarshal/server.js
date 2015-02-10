5af20978e47d58a55bba1ba17d5feb5a3dee616f
fatal0e <joegulisano@gmail.com>
2015-02-10 12:10:01 -0500

    added js file

---
+++ nodejsMarshal/server.js
@@ -0,0 +1,18 @@
+﻿var edge = require('edge');
+
+var dotNetFunction = edge.func('nodejsMarshal.dll');
+
+var payload = {
+	anInteger: 1,
+	aNumber: 3.1415,
+	aString: 'foo',
+	aBoolean: true,
+	aBuffer: new Buffer(10),
+	anArray: [1, 'foo'],
+	anObject: { a: 'foo', b: 12 }
+};
+
+dotNetFunction(12, function (error, result) {
+	if (error) throw error;
+	console.log(result);
+})
