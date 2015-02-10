a5a9cd9e8cc39c604b6bd077b9c41ee5933d6ac9
joegulisano <joegulisano@gmail.com>
2015-02-10 11:39:16 -0500

    initial base

---
+++ nodejsMarshal/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nodejsMarshal
+{
+    public class Startup
+    {
+        public async Task<object> Invoke(object input)
+        {
+            int v = (int)input;
+            return AddSeven(v);
+        }
+
+        private int AddSeven(int number)
+        {
+            return number + 7;
+        }
+    }
+}
